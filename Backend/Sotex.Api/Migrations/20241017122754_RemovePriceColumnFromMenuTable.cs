4e94c5ac2d94fafd381695e54a67188ce7d48da7
NidzaX <59400424+NidzaX@users.noreply.github.com>
2024-10-17 14:31:08 +0200

    Removed price column from menu table
    The price is managed within the Dish class, while the Sides do not require a price.

---
+++ Backend/Sotex.Api/Migrations/20241017122754_RemovePriceColumnFromMenuTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sotex.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePriceColumnFromMenuTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Menus");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Menus",
+                type: "numeric",
+                nullable: true);
+        }
+    }
+}
