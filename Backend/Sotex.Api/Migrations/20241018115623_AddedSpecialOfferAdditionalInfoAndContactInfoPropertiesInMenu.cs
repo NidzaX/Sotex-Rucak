8a19881239c96d801933aefcf5e3efe146a22089
NidzaX <59400424+NidzaX@users.noreply.github.com>
2024-10-18 13:57:56 +0200

    Added migration to accommodate the newly created properties

---
+++ Backend/Sotex.Api/Migrations/20241018115623_AddedSpecialOfferAdditionalInfoAndContactInfoPropertiesInMenu.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sotex.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSpecialOfferAdditionalInfoAndContactInfoPropertiesInMenu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalInfo",
+                table: "Menus",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactInfo",
+                table: "Menus",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SpecialOffer",
+                table: "Menus",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalInfo",
+                table: "Menus");
+
+            migrationBuilder.DropColumn(
+                name: "ContactInfo",
+                table: "Menus");
+
+            migrationBuilder.DropColumn(
+                name: "SpecialOffer",
+                table: "Menus");
+        }
+    }
+}
