dc1346fa680d062dd021604505b2432a84ed6e29
NidzaX <59400424+NidzaX@users.noreply.github.com>
2024-10-30 10:47:13 +0100

    Updated menu.component
    Implemented a method to fetch the menu details and check the isActive and isActiveTomorrow properties. If either is true, skip the upload form and load MenuItemsComponent.

--- Frontend/src/app/dashboard/menu/menu.component.ts
+++ Frontend/src/app/dashboard/menu/menu.component.ts
@@ -2,17 +2,20 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MenuItemsComponent } from "./menu-items/menu-items.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MenuItemsComponent, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
   selectedFile: File | null = null;
   uploadedMenu: any = null;
+  isMenuAvailable: boolean = false;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -21,6 +24,24 @@ export class MenuComponent {
     if(file){
       this.selectedFile = file;
     }
+  }
+
+  checkMenuStatus() {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${localStorage.getItem('authToken')}`
+    });
+
+    this.http
+    .get('http://localhost:5105/api/menus/get-menu-status', {headers})
+      .subscribe({
+        next: (response: any) => {
+          this.isMenuAvailable = response.isActive || response.isActiveTomorrow;
+          if(this.isMenuAvailable) {
+            this.router.navigate(['/dashboard/menu/menu-items']);
+          }
+        },
+        error: (error) => console.error('Error checking menu status:', error)
+      });
   }
 
   uploadMenu() {
