c8685300e4b4abfbbc27bbe900ea6ac863270466
NidzaX <59400424+NidzaX@users.noreply.github.com>
2024-10-31 14:50:37 +0100

    Added refreshPage()

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { LoginComponent } from "./login/login.component";
@@ -10,6 +10,19 @@ import { LoginComponent } from "./login/login.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Frontend';
+
+  ngOnInit(): void {
+    this.refreshPage();
+  }
+
+  refreshPage(){
+   if(!localStorage.getItem('pageRefreshed')) {
+    localStorage.setItem('pageRefreshed', 'true');
+    location.reload();
+   } else {
+    localStorage.removeItem('pageRefreshed');
+   }
+  }
 }
